33d234b7ebb070ceaea4b8cd30323368924acf3c
Oleksandr Korobov <korobov.alex@gmail.com>
2013-04-14 20:40:04 +0300

    Create droid_http.py

---
+++ droid_http.py
@@ -0,0 +1,38 @@
+import SimpleHTTPServer
+import SocketServer
+import subprocess
+
+import android
+
+droid = android.Android()
+
+
+
+PORT = 8001
+
+class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def get_data(self):
+        droid.CameraTakePicture('./photo_.jpg')
+        f = open('./photo_.jpg', 'rb')
+        return f.read()
+        
+    def do_GET(self):
+        try:
+            #send code
+            self.send_response(200)
+
+            #send header first
+            self.send_header('Content-type','image/png')
+            self.end_headers()
+            
+            #send file content to client
+            self.wfile.write(self.get_data())
+            return
+        except IOError:
+            self.send_error(404, 'file not found')
+
+                        
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "serving at port", PORT
+httpd.serve_forever()
